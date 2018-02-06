055812a7b347910040ea91e6e568af7ce227c1bb
debbie <drosenfeld87@gmail.com>
2018-02-06 16:20:08 -0500

    add products

---
+++ products.rb
@@ -0,0 +1,23 @@
+class Products
+
+def initialize(name, base_price, tax)
+
+end
+
+def calc_total
+  return
+end
+
+# new_item Product.new('apple', 'banana,', 'pear')
+  products: [
+    {product: "apple",
+     base_price: 1, tax_rate: 0.25
+    },
+    {product: "pear",
+     base_price: 1.5, tax_rate: 0.25
+    },
+    {product: "banana",
+     base_price: 1, tax_rate: 0.25
+    },
+  ]
+}
