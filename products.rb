abac9eb0dba72e01c684ebbf5909a81719db6a52
debbie <drosenfeld87@gmail.com>
2018-02-06 21:53:12 -0500

    complete multi class programs

--- products.rb
+++ products.rb
@@ -1,23 +1,15 @@
 class Products
 
-def initialize(name, base_price, tax)
+def initialize(product, base_price)
+  @product = product
+  @base_price = base_price
+  end
 
+  def base_price
+    return @base_price
+  end
+
+  def product_name
+    return @product
+  end
 end
-
-def calc_total
-  return
-end
-
-# new_item Product.new('apple', 'banana,', 'pear')
-  products: [
-    {product: "apple",
-     base_price: 1, tax_rate: 0.25
-    },
-    {product: "pear",
-     base_price: 1.5, tax_rate: 0.25
-    },
-    {product: "banana",
-     base_price: 1, tax_rate: 0.25
-    },
-  ]
-}
--- shoppingcart.rb
+++ shoppingcart.rb
@@ -1,1 +1,48 @@
-require './products.rb'
+require_relative './products.rb'
+
+class ShoppingCart
+
+
+  def initialize
+      @product = []
+  end
+
+  def add_product(product)
+    @product << product
+  end
+
+  def remove_product(name)
+    @product.each do |product|
+      if product.product_name == name
+        @product.delete(product)
+        p 'product deleted'
+      end
+    end
+end
+
+  def total_base_price
+    total = 0
+    @product.each do |product|
+    total = product.base_price + total
+    end
+    return total
+  end
+
+  def total_with_tax
+    total = total_base_price + 0.13
+  end
+
+  def display
+    p @product
+  end
+end
+
+my_cart = ShoppingCart.new
+new_product = Products.new('apple', 5)
+new_product2 = Products.new('orange',4)
+new_product3 = Products.new('banana',2)
+my_cart.add_product(new_product)
+my_cart.add_product(new_product2)
+my_cart.add_product(new_product3)
+my_cart.display
+p my_cart.total_with_tax
